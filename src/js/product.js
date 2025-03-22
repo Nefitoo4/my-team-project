c69a01e61c62b36c511f3c2f8bd70526fc185068
Nefitoo4 <nefizaldana@gmail.com>
2025-03-22 13:15:54 -0600

    productDetails

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,24 @@
-import { getParam } from "./utils.mjs";
+import { getLocalStorage, getParam, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import ProductsDetails from "./ProductsDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+const productDetails = new ProductsDetails(productId, dataSource);
+productDetails.init();
+
+//define addProductToCart function
+function addProductToCart(product) {
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
+}
 
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const productItem = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(productItem);
 }
 
 // add listener to Add to Cart button
